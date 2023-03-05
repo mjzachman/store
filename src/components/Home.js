e0c656cfb0871d5420659a3e2e352f94fb27850c
Matt Zachman <mzachman14@gmail.com>
2023-03-05 16:42:58 -0500

    Style home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../styles/Home.css';
+import sec from '../images/SEC.png';
+import stadium from '../images/stadium.jpg';
 
 
 function Home() {
   return (
-    <div className="Home">
-      <h1> MY HOME </h1>
+    <div className="home">
+      <img src = {stadium} alt = "stadium" id = 'stadium'/>
+      <img src={sec} alt = "SEC Logo" id='sec'/>
     </div>
   );
 }
